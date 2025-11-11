4007ceec238ca14dde07fb122505980e77cd07c0
behruz <jumanazarov>
2025-11-11 16:44:53 +0500

    switche da masala ishlash

--- JavaScript/24-Dars switch-case/switche.js
+++ JavaScript/24-Dars switch-case/switche.js
@@ -1,1 +1,31 @@
-console.log("Switche Case");
+// let day = 8;
+
+// switch (day) {
+//     case 1: console.log("Monday"); break;
+//     case 2: console.log("Tuesday"); break;
+//     case 3: console.log("Wednesday"); break;
+//     case 4: console.log("Thursday"); break;
+//     case 5: console.log("Friday"); break;
+//     case 6: console.log("Saturday"); break;
+//     case 7: console.log("Sunday"); break;
+//     default: console.log("Bunday kun yo'q!");
+// }
+
+
+let month = 9;
+
+switch (month) {
+    case 1: console.log("Qish"); break;
+    case 2: console.log("Qish"); break;
+    case 3: console.log("Bahor"); break;
+    case 4: console.log("Bahor"); break;
+    case 5: console.log("Bahor"); break;
+    case 6: console.log("Yoz"); break;
+    case 7: console.log("Yoz"); break;
+    case 8: console.log("Yoz"); break;
+    case 9: console.log("Kuz"); break;
+    case 10: console.log("Kuz"); break;
+    case 11: console.log("Kuz"); break;
+    case 12: console.log("Qish"); break;
+    default: console.log("bunday oy yoq!")
+}
