0d7c2838d3f97397b8cba41b089a6907e558aae5
behruz <jumanazarov>
2025-11-13 16:35:27 +0500

    js array

---
+++ JavaScript/25-Dars array/array.js
@@ -0,0 +1,21 @@
+let names = ["Bmw", "Supra", "Gtr", "Mustang"];
+
+// console.log(names);
+// console.log(names.length);
+// console.log(names[0]);
+
+// console.log(names);
+
+// names[1] = "User";
+
+// console.log(names);
+
+names.push("Jahongir");
+
+names.unshift("Alixon");
+
+names.shift();
+
+names.pop();
+
+console.log(names);
---
+++ JavaScript/25-Dars array/task.js
@@ -0,0 +1,13 @@
+// let numbers = [25, 36, 96, 2, 58, 74, 9, 1, 5, 63];
+
+// let first = numbers[0];
+// let second = numbers[1]; 
+
+// console.log(first + second);
+
+let numbers = [96, 36];
+
+let first = numbers[96];
+let second = numbers[36];
+
+console.log(first,  second);
