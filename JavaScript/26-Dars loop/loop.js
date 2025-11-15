6c2863f8189e31c6c79bfaf54ad7ee63f8e8995c
behruz <jumanazarov>
2025-11-15 16:35:54 +0500

    for loop started

---
+++ JavaScript/26-Dars loop/loop.js
@@ -0,0 +1,11 @@
+// for (let i = 2; i <=100; i +=2) {
+//     console.log(i + " soni juft");
+// }
+
+// for (let i = 11; i<=23; i +=1) {
+//     console.log(i)
+// }
+let kg = 18;
+for (let i = 1.2; i <= 2; i += 0.2) {
+    console.log((i * kg). toFixed(2) + " kg");
+}
