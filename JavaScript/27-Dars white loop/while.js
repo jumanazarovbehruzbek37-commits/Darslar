81fcbbc89f2a4d14d2778b2232350d877d303a02
behruz <jumanazarov>
2025-11-18 16:32:05 +0500

    while

---
+++ JavaScript/27-Dars white loop/while.js
@@ -0,0 +1,19 @@
+// let a = 10;
+// let b = 38;
+
+// let sum = 0;
+
+// for (let i = a; i <= b; i++) {
+//     sum = sum + i;
+// }
+
+// console.log(sum) 
+
+let names = ["Shohruz", "Guli", "Anaxon", "Shahnoza", "Zulayho", "Dilyor",
+"Behruz", "Shahzod", "Alixon", "Jahongir"];
+
+for (let i = 0; i < names.length; i++) {
+    if (names[i] != "Alixon") {
+        console.log(names[i] + "bek");
+    }
+}
