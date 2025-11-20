ad4d9a0479eadd98dc90650482c4955dba41ace2
behruz <jumanazarov>
2025-11-20 16:33:13 +0500

    takrorlash

--- JavaScript/27-Dars white loop/while.js
+++ JavaScript/27-Dars white loop/while.js
@@ -9,11 +9,48 @@
 
 // console.log(sum) 
 
-let names = ["Shohruz", "Guli", "Anaxon", "Shahnoza", "Zulayho", "Dilyor",
-"Behruz", "Shahzod", "Alixon", "Jahongir"];
+// let names = ["Shohruz", "Guli", "Anaxon", "Shahnoza", "Zulayho", "Dilyor",
+// "Behruz", "Shahzod", "Alixon", "Jahongir"];
 
-for (let i = 0; i < names.length; i++) {
-    if (names[i] != "Alixon") {
-        console.log(names[i] + "bek");
-    }
+// for (let i = 0; i < names.length; i++) {
+//     if (names[i] != "Alixon") {
+//         console.log(names[i] + "bek");
+//     }
+// } 
+// let names = ["Shohruz", "Guli", "Anaxon", "Shahnoza", "Zulayho", "Behruz", "Shahzod", "Alixon", "Jahongir"];
+
+
+// let boys = ["Shohruz", "Behruz", "Shahzod", "Alixon", "Jahongir"];
+
+// for (let i = 0; i < names.length; i++) {
+//     if (boys.includes(names[i])) {
+//         console.log(names[i] + "bek");
+//     } else {
+//         console.log(names[i]);
+//     }
+// }
+
+// let a = 3;
+// let n = 11;
+
+// let k = 1;
+// for (let i = 1; i <= n; i++) {
+// k *= a;
+// }
+// console.log(k);
+
+// let a = 2;
+// let b = 10;
+// let s = 0;
+
+// for (let i = a; i <= b; i++) {
+//     s += i * i;
+// }
+// console.log(s);
+
+let n = 5;
+
+for(let i = 1; i <= n; i++) {
+    s = s + (1/i)
 }
+console.log(s)
